eb300185dc0cdd17a3f4e176fe55a5451e618d34
Taylor Robbins (Home) <robbitay@gmail.com>
2021-09-24 22:23:30 -0700

    Fixed a bunch of functions that should have been taking MyStr_t by value rather than const MyStr_t*. Added more helper functions and things to gy_string.h like UnescapeQuotedStringInPlace or ...InArena. Added a function that checks if a string is made up of "valid" identifier characters (like you would see in a C function name)

--- gy_string.h
+++ gy_string.h
@@ -32,6 +32,13 @@ MyStr_t NewStr(u64 length, char* pntr)
 	result.pntr = pntr;
 	return result;
 }
+MyStr_t NewStr(u64 length, const char* pntr)
+{
+	MyStr_t result;
+	result.length = length;
+	result.pntr = (char*)pntr;
+	return result;
+}
 MyStr_t NewStr(char* nullTermStr)
 {
 	MyStr_t result;
@@ -47,13 +54,30 @@ MyStr_t NewStr(const char* nullTermStr)
 	return result;
 }
 
+#define MyStr_Empty NewStr((u64)0, (char*)nullptr)
+
+bool IsStrEmpty(MyStr_t target)
+{
+	Assert(target.pntr != nullptr || target.length == 0);
+	return (target.length == 0);
+}
+bool IsStrNullTerminated(const MyStr_t* target)
+{
+	NotNull(target);
+	if (target->pntr == nullptr) { return false; }
+	return (target->pntr[target->length] == '\0');
+}
+
 // +--------------------------------------------------------------+
 // |                       Assertion Macros                       |
 // +--------------------------------------------------------------+
-#define NotNullStr(strPntr) NotNull2(strPntr, strPntr->pntr)
-#define NotNullStr_(strPntr) NotNull2_(strPntr, strPntr->pntr)
-#define NotEmptyStr(strPntr) Assert(strPntr != nullptr && strPntr->pntr != nullptr && strPntr->pntr[0] != '\0')
-#define NotEmptyStr_(strPntr) Assert_(strPntr != nullptr && strPntr->pntr != nullptr && strPntr->pntr[0] != '\0')
+#define NotNullStr(strPntr)   Assert ((strPntr) != nullptr && ((strPntr)->pntr != nullptr || (strPntr)->length == 0))
+#define NotNullStr_(strPntr)  Assert_((strPntr) != nullptr && ((strPntr)->pntr != nullptr || (strPntr)->length == 0))
+#define NotEmptyStr(strPntr)  Assert ((strPntr) != nullptr && (strPntr)->pntr != nullptr && (strPntr)->length > 0)
+#define NotEmptyStr_(strPntr) Assert_((strPntr) != nullptr && (strPntr)->pntr != nullptr && (strPntr)->length > 0)
+
+#define AssertNullTerm(strPntr)  Assert(IsStrNullTerminated(strPntr))
+#define AssertNullTerm_(strPntr) Assert_(IsStrNullTerminated(strPntr))
 
 // +--------------------------------------------------------------+
 // |                       MemArena Macros                        |
@@ -95,29 +119,60 @@ MyStr_t StrSubstringLength(MyStr_t* target, u64 startIndex, u64 length)
 	return result;
 }
 
-//TODO: These currently don't support unicode's idea of "lowercase/uppercase" equivalent characters
-i32 StrCompareIgnoreCase(const MyStr_t* str1, const MyStr_t* str2, u64 compareLength)
+bool StrEquals(MyStr_t target, MyStr_t comparison)
 {
-	NotNullStr(str1);
-	NotNullStr(str2);
+	NotNullStr(&target);
+	NotNullStr(&comparison);
+	if (target.pntr == nullptr && comparison.pntr == nullptr) { return true; }
+	if (target.length != comparison.length) { return false; }
+	if (target.length == 0 && comparison.length == 0) { return true; }
+	if (target.pntr == nullptr || comparison.pntr == nullptr) { AssertMsg(false, "this case is only for malformed MyStr_t structs (size != 0 but pntr is nullptr)"); return false; }
+	return (MyStrCompare(target.pntr, comparison.pntr, target.length) == 0);
+}
+bool StrEquals(MyStr_t target, const char* comparisonNt)
+{
+	MyStr_t comparisonStr = NewStr(comparisonNt);
+	return StrEquals(target, comparisonStr);
+}
+bool StrEquals(MyStr_t target, u64 comparisonLength, const char* comparisonPntr)
+{
+	MyStr_t comparisonStr = NewStr(comparisonLength, comparisonPntr);
+	return StrEquals(target, comparisonStr);
+}
+bool StrEquals(const char* comparisonNt, MyStr_t target)
+{
+	MyStr_t comparisonStr = NewStr(comparisonNt);
+	return StrEquals(comparisonStr, target);
+}
+bool StrEquals(u64 comparisonLength, const char* comparisonPntr, MyStr_t target)
+{
+	MyStr_t comparisonStr = NewStr(comparisonLength, comparisonPntr);
+	return StrEquals(comparisonStr, target);
+}
+
+//TODO: These currently don't support unicode's idea of "lowercase/uppercase" equivalent characters
+i32 StrCompareIgnoreCase(MyStr_t str1, MyStr_t str2, u64 compareLength)
+{
+	NotNullStr(&str1);
+	NotNullStr(&str2);
 	for (u64 cIndex = 0; cIndex < compareLength; cIndex++)
 	{
-		if (cIndex >= str1->length && cIndex >= str2->length)
+		if (cIndex >= str1.length && cIndex >= str2.length)
 		{
 			return 0; //they were equal all the way up until they both ended early
 		}
-		else if (cIndex >= str1->length)
+		else if (cIndex >= str1.length)
 		{
 			return -1; //str1 ended earlier than str2
 		}
-		else if (cIndex >= str2->length)
+		else if (cIndex >= str2.length)
 		{
 			return 1; //str2 ended earlier than str1
 		}
 		else
 		{
-			char char1 = GetLowercaseAnsiiChar(str1->pntr[cIndex]);
-			char char2 = GetLowercaseAnsiiChar(str2->pntr[cIndex]);
+			char char1 = GetLowercaseAnsiiChar(str1.pntr[cIndex]);
+			char char2 = GetLowercaseAnsiiChar(str2.pntr[cIndex]);
 			if (char1 > char2)
 			{
 				return 1; //str1 comes after str2 alphabetically (str1 > str2)
@@ -131,21 +186,21 @@ i32 StrCompareIgnoreCase(const MyStr_t* str1, const MyStr_t* str2, u64 compareLe
 	}
 	return 0;
 }
-i32 StrCompareIgnoreCase(const MyStr_t* str1, const MyStr_t* str2)
+i32 StrCompareIgnoreCase(MyStr_t str1, MyStr_t str2)
 {
-	if (str1->length > str2->length) { return 1; }
-	if (str1->length < str2->length) { return -1; }
-	return StrCompareIgnoreCase(str1, str2, str1->length);
+	if (str1.length > str2.length) { return 1; }
+	if (str1.length < str2.length) { return -1; }
+	return StrCompareIgnoreCase(str1, str2, str1.length);
 }
-i32 StrCompareIgnoreCase(const MyStr_t* str1, const char* nullTermStr, u64 compareLength)
+i32 StrCompareIgnoreCase(MyStr_t str1, const char* nullTermStr, u64 compareLength)
 {
 	MyStr_t str2 = NewStr(nullTermStr);
-	return StrCompareIgnoreCase(str1, &str2, compareLength);
+	return StrCompareIgnoreCase(str1, str2, compareLength);
 }
-i32 StrCompareIgnoreCase(const MyStr_t* str1, const char* nullTermStr)
+i32 StrCompareIgnoreCase(MyStr_t str1, const char* nullTermStr)
 {
 	MyStr_t str2 = NewStr(nullTermStr);
-	return StrCompareIgnoreCase(str1, &str2);
+	return StrCompareIgnoreCase(str1, str2);
 }
 i32 StrCompareIgnoreCase(const char* str1, const char* str2, u64 compareLength)
 {
@@ -183,28 +238,28 @@ i32 StrCompareIgnoreCase(const char* str1, const char* str2, u64 compareLength)
 	return 0;
 }
 
-MyStr_t* SplitString(MemArena_t* memArena, const MyStr_t* target, const MyStr_t* delineator, u64* numPiecesOut = nullptr, bool ignoreCase = false)
+MyStr_t* SplitString(MemArena_t* memArena, MyStr_t target, MyStr_t delineator, u64* numPiecesOut = nullptr, bool ignoreCase = false)
 {
 	NotNull(memArena);
-	NotNullStr(target);
-	NotEmptyStr(delineator);
+	NotNullStr(&target);
+	NotEmptyStr(&delineator);
 	
 	u64 numPieces = 0;
 	u64 prevDelineator = 0;
-	for (u64 cIndex = 0; cIndex <= target->length+1 - delineator->length; cIndex++)
+	for (u64 cIndex = 0; cIndex <= target.length+1 - delineator.length; cIndex++)
 	{
 		bool match = false;
-		if (cIndex + delineator->length > target->length) { match = true; }
-		else if (ignoreCase && StrCompareIgnoreCase(&target->pntr[cIndex], delineator->pntr, delineator->length) == 0) { match = true; }
-		else if (!ignoreCase && MyStrCompare(&target->pntr[cIndex], delineator->pntr, delineator->length) == 0) { match = true; }
+		if (cIndex + delineator.length > target.length) { match = true; }
+		else if (ignoreCase && StrCompareIgnoreCase(&target.pntr[cIndex], delineator.pntr, delineator.length) == 0) { match = true; }
+		else if (!ignoreCase && MyStrCompare(&target.pntr[cIndex], delineator.pntr, delineator.length) == 0) { match = true; }
 		if (match)
 		{
 			numPieces++;
-			prevDelineator = cIndex + delineator->length;
-			cIndex += delineator->length-1;
+			prevDelineator = cIndex + delineator.length;
+			cIndex += delineator.length-1;
 		}
 	}
-	AssertMsg(prevDelineator >= target->length, "We missed a piece at the end somehow? We should be going 1 past the end and considering it a match");
+	AssertMsg(prevDelineator >= target.length, "We missed a piece at the end somehow? We should be going 1 past the end and considering it a match");
 	
 	if (numPieces == 0)
 	{
@@ -216,21 +271,21 @@ MyStr_t* SplitString(MemArena_t* memArena, const MyStr_t* target, const MyStr_t*
 	
 	u64 pIndex = 0;
 	prevDelineator = 0;
-	for (u64 cIndex = 0; cIndex <= target->length+1 - delineator->length; cIndex++)
+	for (u64 cIndex = 0; cIndex <= target.length+1 - delineator.length; cIndex++)
 	{
 		bool match = false;
-		if (cIndex + delineator->length > target->length) { match = true; }
-		else if (ignoreCase && StrCompareIgnoreCase(&target->pntr[cIndex], delineator->pntr, delineator->length) == 0) { match = true; }
-		else if (!ignoreCase && MyStrCompare(&target->pntr[cIndex], delineator->pntr, delineator->length) == 0) { match = true; }
+		if (cIndex + delineator.length > target.length) { match = true; }
+		else if (ignoreCase && StrCompareIgnoreCase(&target.pntr[cIndex], delineator.pntr, delineator.length) == 0) { match = true; }
+		else if (!ignoreCase && MyStrCompare(&target.pntr[cIndex], delineator.pntr, delineator.length) == 0) { match = true; }
 		if (match)
 		{
 			Assert(pIndex < numPieces);
 			Assert(cIndex >= prevDelineator);
-			results[pIndex].pntr = &target->pntr[prevDelineator];
+			results[pIndex].pntr = &target.pntr[prevDelineator];
 			results[pIndex].length = cIndex - prevDelineator;
 			pIndex++;
-			prevDelineator = cIndex + delineator->length;
-			cIndex += delineator->length-1;
+			prevDelineator = cIndex + delineator.length;
+			cIndex += delineator.length-1;
 		}
 	}
 	AssertMsg(pIndex == numPieces, "Expected the same number of pieces in both runs through the loop");
@@ -238,21 +293,156 @@ MyStr_t* SplitString(MemArena_t* memArena, const MyStr_t* target, const MyStr_t*
 	if (numPiecesOut != nullptr) { *numPiecesOut = numPieces; }
 	return results;
 }
-MyStr_t* SplitString(MemArena_t* memArena, const MyStr_t* target, const char* delineatorNt, u64* numPiecesOut = nullptr, bool ignoreCase = false)
+MyStr_t* SplitString(MemArena_t* memArena, MyStr_t target, const char* delineatorNt, u64* numPiecesOut = nullptr, bool ignoreCase = false)
 {
 	MyStr_t delineator = NewStr(delineatorNt);
-	return SplitString(memArena, target, &delineator, numPiecesOut, ignoreCase);
+	return SplitString(memArena, target, delineator, numPiecesOut, ignoreCase);
 }
-MyStr_t* SplitString(MemArena_t* memArena, const char* targetNt, const MyStr_t* delineator, u64* numPiecesOut = nullptr, bool ignoreCase = false)
+MyStr_t* SplitString(MemArena_t* memArena, const char* targetNt, MyStr_t delineator, u64* numPiecesOut = nullptr, bool ignoreCase = false)
 {
 	MyStr_t target = NewStr(targetNt);
-	return SplitString(memArena, &target, delineator, numPiecesOut, ignoreCase);
+	return SplitString(memArena, target, delineator, numPiecesOut, ignoreCase);
 }
 MyStr_t* SplitString(MemArena_t* memArena, const char* targetNt, const char* delineatorNt, u64* numPiecesOut = nullptr, bool ignoreCase = false)
 {
 	MyStr_t target = NewStr(targetNt);
 	MyStr_t delineator = NewStr(delineatorNt);
-	return SplitString(memArena, &target, &delineator, numPiecesOut, ignoreCase);
+	return SplitString(memArena, target, delineator, numPiecesOut, ignoreCase);
+}
+
+//TODO: This doesn't play SUPER nice with unicode strings. It shouldn't really "fail" though
+//TODO: Add support for hex or unicode sequences
+//Returns the number of bytes that the string shrunk by after unescaping
+u64 UnescapeQuotedStringInPlace(MyStr_t* target, bool removeQuotes = true, bool allowNewLineEscapes = true, bool allowOtherEscapeCodes = false)
+{
+	u64 numBytesSmaller = 0;
+	u64 writeIndex = 0;
+	for (u64 readIndex = 0; readIndex < target->length; readIndex++)
+	{
+		char currChar = target->pntr[readIndex];
+		char nextChar = (readIndex+1 < target->length) ? target->pntr[readIndex+1] : '\0';
+		if (currChar == '\\')
+		{
+			if (nextChar == '\\')
+			{
+				target->pntr[writeIndex] = '\\';
+				writeIndex++;
+				numBytesSmaller++;
+				readIndex++;
+			}
+			else if (nextChar == '\"')
+			{
+				target->pntr[writeIndex] = '\"';
+				writeIndex++;
+				numBytesSmaller++;
+				readIndex++;
+			}
+			else if (nextChar == '\'')
+			{
+				target->pntr[writeIndex] = '\'';
+				writeIndex++;
+				numBytesSmaller++;
+				readIndex++;
+			}
+			else if (nextChar == 't')
+			{
+				target->pntr[writeIndex] = '\t';
+				writeIndex++;
+				numBytesSmaller++;
+				readIndex++;
+			}
+			else if (nextChar == 'n' && allowNewLineEscapes)
+			{
+				target->pntr[writeIndex] = '\n';
+				writeIndex++;
+				numBytesSmaller++;
+				readIndex++;
+			}
+			else if (nextChar == 'r' && allowNewLineEscapes)
+			{
+				target->pntr[writeIndex] = '\r';
+				writeIndex++;
+				numBytesSmaller++;
+				readIndex++;
+			}
+			else if (nextChar == 'b' && allowOtherEscapeCodes)
+			{
+				target->pntr[writeIndex] = '\b';
+				writeIndex++;
+				numBytesSmaller++;
+				readIndex++;
+			}
+			else if (nextChar == 'a' && allowOtherEscapeCodes)
+			{
+				target->pntr[writeIndex] = '\a';
+				writeIndex++;
+				numBytesSmaller++;
+				readIndex++;
+			}
+			else if (nextChar == 'b' && allowOtherEscapeCodes)
+			{
+				target->pntr[writeIndex] = '\b';
+				writeIndex++;
+				numBytesSmaller++;
+				readIndex++;
+			}
+			else if (nextChar == 'f' && allowOtherEscapeCodes)
+			{
+				target->pntr[writeIndex] = '\f';
+				writeIndex++;
+				numBytesSmaller++;
+				readIndex++;
+			}
+			else if (nextChar == 'v' && allowOtherEscapeCodes)
+			{
+				target->pntr[writeIndex] = '\v';
+				writeIndex++;
+				numBytesSmaller++;
+				readIndex++;
+			}
+			else if (nextChar == '?' && allowOtherEscapeCodes)
+			{
+				target->pntr[writeIndex] = '\?';
+				writeIndex++;
+				numBytesSmaller++;
+				readIndex++;
+			}
+			else
+			{
+				//Invalid escape code
+				//Just leave this escape sequence
+				target->pntr[writeIndex] = target->pntr[readIndex];
+				writeIndex++;
+			}
+		}
+		else if (currChar == '\"' && (readIndex == 0 || readIndex == target->length-1) && removeQuotes)
+		{
+			numBytesSmaller++;
+			//don't increment writeIndex
+		}
+		else
+		{
+			target->pntr[writeIndex] = target->pntr[readIndex];
+			writeIndex++;
+		}
+	}
+	Assert(numBytesSmaller < target->length);
+	Assert(numBytesSmaller == target->length - writeIndex);
+	target->length -= numBytesSmaller;
+	if (numBytesSmaller > 0) 
+	{
+		//If we shrunk at all then we should have space to insert a new null-term char
+		//Otherwise we rely on the null-term pre-existing if the string didn't change at all
+		target->pntr[target->length] = '\0';
+	}
+	return numBytesSmaller;
+}
+MyStr_t UnescapeQuotedStringInArena(MemArena_t* memArena, MyStr_t target, bool removeQuotes = true, bool allowNewLineEscapes = true, bool allowOtherEscapeCodes = false)
+{
+	NotNull(memArena);
+	MyStr_t result = AllocString(memArena, &target);
+	u64 numBytesSmaller = UnescapeQuotedStringInPlace(&result, removeQuotes, allowNewLineEscapes, allowOtherEscapeCodes);
+	return result;
 }
 
 #endif //  _GY_STRING_H
--- gy_unicode.h
+++ gy_unicode.h
@@ -20,4 +20,43 @@ char GetUppercaseAnsiiChar(char c)
 	return c;
 }
 
+//NOTE: This technically works 'okay' for unicode strings, it's a bit strict in which characters are valid
+bool IsStringValidIdentifier(u64 strLength, const char* strPntr, bool allowUnderscores = true, bool allowNumbers = true, bool allowLeadingNumbers = false, bool allowEmpty = false, bool allowSpaces = false)
+{
+	Assert(strPntr != nullptr || strLength == 0);
+	if (strLength == 0)
+	{
+		return allowEmpty;
+	}
+	
+	for (u64 cIndex = 0; cIndex < strLength; cIndex++)
+	{
+		char nextChar = strPntr[cIndex];
+		if ((nextChar >= 'a' && nextChar <= 'z') ||
+			(nextChar >= 'A' && nextChar <= 'Z'))
+		{
+			continue;
+		}
+		else if (nextChar >= '0' && nextChar <= '9' && allowNumbers)
+		{
+			if (cIndex == 0 && !allowLeadingNumbers) { return false; }
+			continue;
+		}
+		else if (nextChar == '_' && allowUnderscores)
+		{
+			continue;
+		}
+		else if (nextChar == ' ' && allowSpaces)
+		{
+			continue;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	return true;
+}
+
 #endif //  _GY_UNICODE_H
