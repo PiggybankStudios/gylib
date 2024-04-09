c839af2babe6b6167e7ca9b038ce8b073b689546
Taylor Robbins (Piggybank Studios) <PrincessDungeonSmasher@gmail.com>
2024-04-09 10:49:40 -0700

    Added ExtractDefine.py which helps the build.bat extract #define lines in a header file and use them to inform the compilation options.

---
+++ ExtractDefine.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+import os.path
+import sys
+import re
+
+# This file is script is expected to be used in a batch script like so:
+# for /f "delims=" %%value in ('python ExtractDefines.py ../../../game/pig_config.h DEFINE_NAME') do set DEFINE_NAME=%%value
+
+filePath = sys.argv[1];
+targetDefineName = sys.argv[2];
+fileBase, fileName = os.path.split(filePath);
+# print("Editing file \"" + filePath + "\"");
+
+headerFile = open(filePath, "r");
+
+fileContents = headerFile.read();
+# print("File Contents: \"" + str(fileContents) + "\"");
+headerFile.close();
+
+searchStr = "\\#define[ \\t]+([A-Za-z_][A-Za-z0-9_]*)[ \\t]*(\\d+)";
+
+matches = re.findall(searchStr, fileContents);
+# print("Found %d #defines in %s" % (len(matches), fileName))
+
+foundMatch = False;
+for match in matches:
+#
+	if (match[0] == targetDefineName):
+	#
+		# print("Found #define %s %s" % (match[0], match[1]));
+		print(match[1]);
+		foundMatch = True;
+		break;
+	#
+#
+
+if (not foundMatch):
+#
+	print("Couldn't find #define %s in %s" % (targetDefineName, fileName), file=sys.stderr);
+#
